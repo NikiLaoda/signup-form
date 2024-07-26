f7c0410f68cd9d817d1bb97dc151980fae839f27
Nikita Pustovalov <workpagelaoda@gmail.com>
2024-07-26 18:03:36 +0300

    JS checking password implemented

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function checkFunc() {
+function checkPass() {
     let password = document.querySelector("#pass-word");
     let confirmPass = document.querySelector("#confirm-password");
-    
+
+    if (password.value !== confirmPass.value) alert("Passwords are not equal!");
 }
